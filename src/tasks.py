7e8571c0522d94438922976c51751a7d55464d46
Josephine Nampala <nampalajosephine@gmail.com>
2018-09-20 10:58:26 +0300

    add files

---
+++ src/tasks.py
@@ -0,0 +1,54 @@
+# This file contains code that manages your todo_list
+todo_list = [] #List containing the tasks to be done
+finished_task = '[Finished]'
+    
+
+# Write a function creates a task
+
+
+def create_task(task):
+    """
+    Adds the task (string value) to todo_list
+    """
+    task = input("State your new task: ")#providing placeholder for inputting new tasks
+  
+    #adding created new tasks to the todo_list using the append function
+   
+    todo_list.append(task)
+    return todo_list
+    
+
+# Write a function deletes a task
+
+
+def delete_task(task):
+    """
+    Removes the specified task from the todo_list
+    """
+    todo_list.remove(task)
+    return todo_list
+
+# Write a function that marks a task finished
+
+
+def mark_as_finished(task):
+    """
+    Append the string label '[finished]' at the end of the task 
+    if it does not already have the label appended.
+    It should remain in the todo_list
+    """
+    finished_task = '[Finished]'
+    task = "task{0}[Finished]".format(task)#Adds string '[Finished] at the end of finshed task.
+    return todo_list
+       
+       
+
+# Write a function deletes all tasks
+
+
+def delete_all_tasks():
+    """
+    Empty the the todo_lsit
+    """
+    todo_list.clear()
+    return todo_list
