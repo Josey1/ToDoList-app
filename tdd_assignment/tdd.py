46c0f29864a3d54800dad22b0661c4ad716a8c1f
Josephine Nampala <nampalajosephine@gmail.com>
2018-09-28 00:50:24 +0300

    continue with assignment

--- tdd_assignment/tdd.py
+++ tdd_assignment/tdd.py
@@ -2,11 +2,19 @@
 user_accounts = {}
 expected_char = ["!", "@","%", "#", "&", "*", "$", "^"]
 
+numbers = string.digits
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+
+password = "@Jose1"
+
+
 def register_user(username, email, password):
     """
     This function allows a user register/create an account
     """
     pass
+
 
 def userprofile(name, age, gender):
     """
@@ -14,11 +22,13 @@ def userprofile(name, age, gender):
     """
     pass
 
+
 def add_account(username, password):
    """
    Adds the key value pair to the user_accounts dictionaries above
    """
    pass
+
 
 def login(username, password):
     """
@@ -26,6 +36,7 @@ def login(username, password):
     accounts dicitionary
     """
     pass
+    
 
 def validate_password(password):
     if not len(password) is >= 4:
@@ -34,6 +45,13 @@ def validate_password(password):
     for expected_char in expected_chars:
        if not contains_char(password, expected_char):
            return False, "does not contain {0}".format(expected_char)
+
+    for l in password:
+		if password[0] in upper or l in lower or l in number:
+			return True
+		else:
+			return False
+	
     
 
 def validate_email(email):
@@ -46,9 +64,19 @@ def validate_email(email):
 def validate_name(name):
     if  name.isalpha() == False:
        print(" the name should contain only alphabetic letters") 
-    
+
+def validate_usename(username):
+    if username.isalpha() == False:
+        print("username should contain only alphabetic letters")
+
+    if username == name:
+        print("The username should be different from the name")
+
+
 def validate_age(age):
-    
+    if isinstance(age, str) or age == 0:
+        return False
+    return True
 
     
     
--- tdd_assignment/test_tdd.py
+++ tdd_assignment/test_tdd.py
@@ -1,18 +1,33 @@
 import unittest
-from validation import validation
+from unittest import TestCase
+
+from tdd.py import validate_password
+from tdd.py import validate_email
+from tdd.py import validate_name
+from tdd.py import validate_username
+from tdd.py import validate_age
 
 
-class TestValidation(unittest.TestCase):
+class TestValidation(TestCase):
+    """Tests validity of user input"""
+    def test_password_contains_uppercase_letter(self):
+        self.assertTrue(validate_password('@Jose1'))
+        self.assertFalse(validate_password('@Cici'))
 
-    def test_username(self):
-        username = " "
-        self.assertTrue(tdd.user_valiidation(test_username))
+   
+    def test_password_contains_lowercase_letter(self):
+        self.assertTrue(validate_password('@Jose1'))
+        self.assertFalse(validate_password('@Cici'))
 
-    def test_password(self):
-        password = " "
-        self.assertTrue(tdd.user_validation(password))
+    
+    def test_password_contains_a_digit(self):
+        self.assertTrue(validate_password('@Jose1'))
+        self.assertFalse(validate_password('@Cici'))
+
+    
+    def test_password_contains_special_character(self):
+        self.assertTrue(validate_password('@Jose1))
+        self.assertFalse(validate_password('@Cici'))
 
 if __name__ == "__main__":
- while(True):
-     break
-     name = input("wrong ")
+    pass
